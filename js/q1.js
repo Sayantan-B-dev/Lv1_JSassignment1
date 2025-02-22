c402ac290d9f0649840de29ad44310ecf56910e8
Sayantan <sayantanbharati611@gmail.com>
2025-02-22 20:07:21 +0530

    Js basic assignments Live cohort

---
+++ js/q1.js
@@ -0,0 +1,3 @@
+// Age Category Message
+// Ask the user for their age. If they are under 18, print “You are a minor.” If they are between 18 and 60, print “You are an adult.” If they are above 60, print “You are a senior citizen.”
+
