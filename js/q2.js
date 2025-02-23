2e6bad3848600ca4b5cd336b73d0e1aaf2265f5a
Sayantan <sayantanbharati611@gmail.com>
2025-02-23 20:55:26 +0530

    All done for now

--- js/q2.js
+++ js/q2.js
@@ -4,7 +4,7 @@ let n1=Number(prompt("Enter the first number"));
 let n2=Number(prompt("Enter the second number"));
 if(typeof(n1)=="number" && typeof(n2)=="number" && !isNaN(n1) && !isNaN(n2)){
     let sum=((n1+n2)%2)==0?"even":"odd";
-    alert("Sum of the given number is "+sum);
+    alert("Sum of the given numbers is "+sum);
 }else{
     alert("enter a valid number please")
 }
